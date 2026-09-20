6cbd4caaae1c824e2078f9b0154f17ae26129506
ernestomontalvo49@gmail.com <ernestomontalvo49@gmail.com>
2026-09-20 15:20:24 -0600

    Banderas

---
+++ app/src/main/java/com/example/banderas/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.banderas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.banderas.ui.theme.BanderasTheme
+
+class MainActivity : ComponentActivity() {
+    //signo de pregunta posiblemente puede ser nula, no es recomendable
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BanderasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "hoaksaka $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BanderasTheme {
+        Greeting("no")
+    }
+}
